762be02a862da9ee161d94b06b8af771dd43d195
Adit Pareek <aditepic10@gmail.com>
2020-07-09 15:07:44 -0400

    added documentation

--- src/bot.py
+++ src/bot.py
@@ -1,17 +1,33 @@
 import os
 
 import discord  # pip install discord.py
-from dotenv import load_dotenv
+from dotenv import load_dotenv  # .env for discord token
 
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+load_dotenv()  # load .env for discord token
+TOKEN = os.getenv('DISCORD_TOKEN')  # extract discord token
 
-client = discord.Client()
+client = discord.Client()  # open up discord client
+
+"""Checks if bot is ready and prints a message"""
 
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+
+
+"""Checks for messages and responds based on word count"""
+# TODO: Forward to chair's DMs and only allow successful directive requests within 10 mins of the first
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    words = len(message.content.split(" "))  # get string contents and split by space to get word count
+    if words > 100:
+        response = "Sorry, your directive is too long. Please shorten it and send another message"
+    else:
+        response = "Thank you, your directive has been received."
+    await message.channel.send(response)
 
 
 client.run(TOKEN)
